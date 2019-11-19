9a791af174d12e48c949989343b514e9f0e976c9
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2019-11-19 06:59:39 -0300

    add arquivo database.js

--- exercises-js/06_node/projectAPI/src/server.js
+++ exercises-js/06_node/projectAPI/src/server.js
@@ -4,15 +4,23 @@ const port = 3003;
 const express = require('express');
 const app = express();
 
-app.get('/products',(req, res,next)=>{
-    console.log("Middleware 1...")
-    next();
-})
+const database = require('./database')
 
 app.get('/products', (req, res, next)=>{
-    res.send({name: 'Notebook', price: 123.45}); //Converte para JSON automaticamente
+    res.send(database.getProducts()); //Converte para JSON automaticamente
 })
 
+app.get('/products/:id', (req, res, next)=>{
+    res.send(database.getProduct(req.params.id))
+})
+
+app.post('/products',(req, res, next)=>{
+    const product = database.saveProduct({
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product)
+})
 
 app.listen(port, ()=>{      //Iniciando porta
     console.log(`Servidor executando na porta ${port}`);
