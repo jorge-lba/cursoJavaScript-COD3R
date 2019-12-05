cec5a9e66a578612b677c303792641f7ffc7440f
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2019-12-04 22:40:45 -0300

    Aulas sobre formularios

---
+++ exercises-js/09_HTML/execises/forms/server.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/usuarios', (req,res) =>{
+    console.log(req.body);
+    res.send('<h1>Parabéns. Usuário Incluido!</h1>');
+});
+
+app.post('/usuarios/:id', (req,res) =>{
+    console.log(req.params.id);
+    console.log(req.body);
+
+    res.send('<h1>Parabéns. Usuário Alterado!</h1>');
+});
+
+app.listen(3003);
